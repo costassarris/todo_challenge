2becda56eac7195f062fd065dc46b783bda599c7
costassarris <costassarris@gmail.com>
2015-03-23 15:40:20 +0000

    tasks in list can now be edited

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var toDo = angular.module('ToDo', []);
+var toDo = angular.module('ToDo', ["xeditable"]);
+
+toDo.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
